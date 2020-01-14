6305c07fec21d76098e8ce767eb18f2ff1abd6e1
andreavitaletti <vitaletti@dis.uniroma1.it>
2020-01-14 11:47:56 +0100

    some minor changes

---
+++ it.search-data.min.3b6c3bb6a26d7f346e60bd8601d2d71b8c743d62510656b3419d7807cad3eff6.js
@@ -0,0 +1,1 @@
+'use strict';(function(){const indexCfg={cache:true};indexCfg.doc={id:'id',field:['title','content'],store:['title','href'],};const index=FlexSearch.create('balance',indexCfg);window.bookSearchIndex=index;index.add({'id':0,'href':'/it/','title':"Andrea Vitaletti",'content':"In Italiano Ci sto lavorando su \u0026hellip; work in prgress!\n"});})();
---
+++ it.search.min.e584379bb66d0743932a054a5ffba21e07c410948fc38907a9d80b824934e50e.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/it.search-data.min.3b6c3bb6a26d7f346e60bd8601d2d71b8c743d62510656b3419d7807cad3eff6.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
