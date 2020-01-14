a11f4d36b2385b7b7cd167be1eaa19f9ac29229e
andreavitaletti <vitaletti@dis.uniroma1.it>
2020-01-14 11:33:53 +0100

    first release

---
+++ en.search.min.5bc71943fccd63d21192940fa5e314c11ebec6a0e1c47809b3a3631e74c35916.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.db33bcc3692fbf17425bbb5525c77aa261a931fa5b2960b7c67e49011424ee5b.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
